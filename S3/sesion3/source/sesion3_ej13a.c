2f88aabced3afecca5af19b6fa6abea1f8ae42bf
Aniol Martí <aniol.me@gmail.com>
2017-10-30 22:43:31 +0100

    Ex13

--- S3/sesion3/source/sesion3_ej13a.c
+++ S3/sesion3/source/sesion3_ej13a.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-	unsigned int b10, res, quo, b2=0,par=1;
+	unsigned int b10, res, quo, b2=0, pes=1;
 
 	printf("Introduzca un número natural en base 10: ");
 	scanf("%d", &b10);
@@ -9,12 +9,11 @@ int main() {
 
 	while (quo >= 1) {
 		res = quo % 2;
-		b2 = b2 * 10 + res;
-		if (b2 == 0) par = par * 10;
+		b2 = b2 + res * pes;
+		pes = pes * 10;
 		quo = quo / 2;
 	
 	}
-	b2 = b2 * par;
 	printf("%d (base 10) = %d (base 2)\n", b10, b2);
 		
 }
--- S3/sesion3/source/sesion3_ej13b.c
+++ S3/sesion3/source/sesion3_ej13b.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-	unsigned int b10, res, quo, b2=0,par=1, base;
+	unsigned int b10, res, quo, b2=0, base, pes=1;
 
 	printf("Introduzca un número natural en base 10: ");
 	scanf("%d", &b10);
@@ -11,12 +11,11 @@ int main() {
 
 	while (quo >= 1) {
 		res = quo % base;
-		b2 = b2 * 10 + res;
-		if (b2 == 0) par = par * 10;
+		b2 = b2 + res * pes;
+		pes = pes * 10;
 		quo = quo / base;
 	
 	}
-	b2 = b2 * par;
 	printf("%d (base 10) = %d (base %d)\n", b10, b2, base);
 		
 }
